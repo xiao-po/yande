5468c886c67365e844b26f06af40a1c321feca3c
wangly <wangluy@ipharmacare.net>
2019-01-01 16:59:56 +0800

    合并

--- lib/view/allView.dart
+++ lib/view/allView.dart
@@ -3,8 +3,6 @@ library allview;
 export 'index/indexView.dart';
 export 'imageStatus/imageStatusView.dart';
 export 'search/resultView.dart';
-
-export 'search/resultView.dart';
 export 'search/searchDelegate.dart';
 export 'download/downloadView.dart';
 export 'collectList/collectListView.dart';
