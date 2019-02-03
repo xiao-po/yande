83dd3d0a73024e88e79d102b34d39bc909440a2b
wangluy <wangluy@ipharmacare.net>
2019-02-04 00:09:41 +0800

    增加一个util

---
+++ lib/utils/fileUtils.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class FileUitls {
+
+  static Future<Directory> getDirAndCreate(String path) async {
+    Directory dir = new Directory(path);
+    bool isDirExist = await dir.exists();
+    if (!isDirExist) {
+      await dir.create();
+    }
+    return dir;
+  }
+
+
+  static Future<Directory> getExternalDir() async {
+    String myPath = '/Android/data/com.example.yande';
+    Directory dir = await getExternalStorageDirectory();
+    return await getDirAndCreate('${dir.path}$myPath');
+  }
+  static Future<Directory> getExternalDatabaseDir() async {
+    Directory dir = await getExternalDir();
+    return await getDirAndCreate('${dir.path}/databases');
+  }
+
+
+
+}
+
