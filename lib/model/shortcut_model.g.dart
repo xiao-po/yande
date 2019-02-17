0e2b00a79daad71a69cdc1e31024a57cd7edd434
wangluy <wangluy@ipharmacare.net>
2019-02-17 09:53:45 +0800

    提交一份json factory

---
+++ lib/model/shortcut_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shortcut_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ShortcutModel _$ShortcutModelFromJson(Map<String, dynamic> json) {
+  return ShortcutModel(json['keyword'] as String, json['nickname'] as String);
+}
+
+Map<String, dynamic> _$ShortcutModelToJson(ShortcutModel instance) =>
+    <String, dynamic>{
+      'keyword': instance.keyword,
+      'nickname': instance.nickname
+    };
